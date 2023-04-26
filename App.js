aa8176c6f4f0df308363b099b1cc0709efe642ae
eburzlaff <du.di.99@hotmail.com>
2023-04-25 20:12:46 -0500

    Adding Icon component

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import {
   Dimensions,
   Platform
 } from 'react-native';
+
 import { useDeviceOrientation } from '@react-native-community/hooks';
 
 import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -22,12 +23,16 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import Card from './app/components/Card'
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
 
 export default function App() {
   console.log(useDeviceOrientation());
   console.log(Dimensions.get('screen'))
 
   return (
-    <MessagesScreen/>
+    <Screen>
+      <Icon name="email" size={100}/>
+    </Screen>
   );
 }
