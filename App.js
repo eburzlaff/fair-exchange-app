7f5a3e6b52a7513a4a1828113257d1f5b7176f31
eburzlaff <du.di.99@hotmail.com>
2023-06-26 07:57:58 -0500

    Adding LoginScreen and reusable form components as well as index.js for multiple imports in one line

--- App.js
+++ App.js
@@ -33,29 +33,13 @@ import ListItemDeleteAction from './app/components/ListItemDeleteAction';
 import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
-
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothing", value: 2 },
-  { label: "Cameras", value: 3 },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
   console.log(useDeviceOrientation());
   console.log(Dimensions.get('screen'))
 
   return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Category"
-      />
-      <AppTextInput placeholder="Email"/>
-    </Screen>
-    
+    <LoginScreen/>
   );
 }
