87851b491504a03222a4b1f94aef167f03d48f38
eburzlaff <du.di.99@hotmail.com>
2023-06-26 05:04:45 -0500

    Adding AppPicker and PickerItem components

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import { 
   StyleSheet,
   Text,
@@ -30,14 +32,29 @@ import ListItem from './app/components/ListItem';
 import ListItemDeleteAction from './app/components/ListItemDeleteAction';
 import ListingsScreen from './app/screens/ListingsScreen';
 import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
   console.log(useDeviceOrientation());
   console.log(Dimensions.get('screen'))
 
   return (
     <Screen>
-      <AppTextInput/>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput placeholder="Email"/>
     </Screen>
     
   );
