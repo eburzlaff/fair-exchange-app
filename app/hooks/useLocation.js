a5eb6faceb497826b9084e49229dc9a4b7df32bb
eburzlaff <du.di.99@hotmail.com>
2023-07-05 20:02:56 -0500

    Adding API layer to project, animations, ActivityIndicator, UploadScreen and modifying some components and screens

--- app/hooks/useLocation.js
+++ app/hooks/useLocation.js
@@ -2,42 +2,62 @@ import { useEffect, useState } from "react";
 import * as Location from "expo-location";
 
 export default useLocation = () => {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState();
+  const [errorMsg, setErrorMsg] = useState();
 
-  // const getLocation = async () => {
-  //   try {
-  //     const { granted } = await Location.requestBackgroundPermissionsAsync();
-  //     if (!granted) return;
-  //     const {
-  //       coords: { latitude, longitude },
-  //     } = await Location.getLastKnownPositionAsync();
-  //     setLocation({ latitude, longitude });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const getLocation = async () => {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+      if (!granted) return;
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      setLocation({ latitude, longitude });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    (async () => {
-      
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
-
-      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({});
-      setLocation({latitude, longitude});
-    })();
+    getLocation();
   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+  return location;
 
-  return text;
+  // useEffect(() => {
+  //   (async () => {
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //       return;
+  //     }
+  //     const {coords: { latitude, longitude }} = await Location.getCurrentPositionAsync();
+  //     setLocation({ latitude, longitude });
+  //   })();
+  // }, []);
+
+  // return location;
+
+  // useEffect(() => {
+  //   (async () => {
+      
+  //     let { status } = await Location.requestForegroundPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       setErrorMsg('Permission to access location was denied');
+  //       return;
+  //     }
+
+  //     const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({});
+  //     setLocation({latitude, longitude});
+  //   })();
+  // }, []);
+
+  // let text = 'Waiting..';
+  // if (errorMsg) {
+  //   text = errorMsg;
+  // } else if (location) {
+  //   text = JSON.stringify(location);
+  // }
+
+  // return text;
 };
--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
@@ -11,7 +11,9 @@ import {
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import Screen from "../components/Screen";
 import FormImagePicker from "../components/forms/FormImagePicker";
+import listingsApi from "../api/listings";
 import useLocation from "../hooks/useLocation";
+import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
@@ -79,10 +81,38 @@ const categories = [
 ];
 
 function ListingEditScreen() {
-  const locationText = useLocation();
+  const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const handleSubmit = async (listing, { resetForm }) => {
+    
+    console.log(listing, location);
+    setProgress(0);
+    setUploadVisible(true);
+    const result = await listingsApi.addListing(
+      { ...listing, location },
+      (progress) => setProgress(progress)
+    );
+
+    console.log(result.data);
+
+    if (!result.ok) {
+      setUploadVisible(false);
+      return alert("Could not save the listing"); 
+    }
+
+    resetForm();
+
+  };
 
   return (
     <Screen style={styles.container}>
+      <UploadScreen
+        onDone={() => setUploadVisible(false)}
+        progress={progress}
+        visible={uploadVisible}
+      />
       <Form
         initialValues={{
           title: "",
@@ -91,7 +121,7 @@ function ListingEditScreen() {
           category: null,
           images: [],
         }}
-        onSubmit={(values) => console.log(locationText)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images" />
