3eaf9cffc6c43cb77b67d6c28ff885071aeb7fea
eburzlaff <du.di.99@hotmail.com>
2023-07-08 12:56:38 -0500

    Adding OfflineNotice component for when the app is offline

--- App.js
+++ App.js
@@ -22,19 +22,19 @@ import { useDeviceOrientation } from '@react-native-community/hooks'
 
 import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
-import AuthNavigator from "./app/navigation/AuthNavigator";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
 import AppNavigator from "./app/navigation/AppNavigator";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
   console.log(useDeviceOrientation());
   console.log(Dimensions.get('screen'))
 
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
