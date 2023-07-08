d408636ff18b40d770e447058b940fbb80c37a23
eburzlaff <du.di.99@hotmail.com>
2023-07-07 19:30:55 -0500

    Modifying ListingsScreen, ListingDetails and Card components to display thumbnail image while main image is loading. Using console.log statements to try to fix offline caching

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,28 @@
 import { create } from "apisauce";
+import cache from "../utility/cache";
 
 const apiClient = create({
   baseURL: "http://192.168.1.108:9000/api",
 });
+
+const get = apiClient.get;
+apiClient.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
+
+  if (response.ok) {
+    cache.store(url, response.data);
+    console.log("Data stored in cache:", url, response.data);
+    return response;
+  }
+
+  const data = await cache.get(url);
+  if (data) {
+    console.log("Data retrieved from cache:", url, data);
+    return { ok: true, data };
+  } else {
+    console.log("Cache miss, requesting data from server:", url);
+    return response;
+  }
+};
 
 export default apiClient;
